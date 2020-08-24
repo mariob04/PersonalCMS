322b7692bef02ca02b554a3be392605a2daadaab
Mario Bilic <mario.bilic4@gmail.com>
2020-08-24 17:31:16 +0200

    Add Admin control and login functionality

---
+++ PersonalCMS/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PersonalCMS.Data;
+
+[assembly: HostingStartup(typeof(PersonalCMS.Areas.Identity.IdentityHostingStartup))]
+namespace PersonalCMS.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
